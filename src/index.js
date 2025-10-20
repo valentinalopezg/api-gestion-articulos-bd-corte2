b1d7ac099b3eadc88c42fe20c711af841dbf8ffc
valentinalopezgrc <lavaloga17@gmail.com>
2025-10-20 15:14:12 -0500

    Estructura inicial del proyecto y configuración base
    - Se inicializó el proyecto con Node.js y Express.
    - Se instaló nodemon para entorno de desarrollo.
    - Se creó la base de datos 'bd_corte2' y la colección 'articulos' en MongoDB Atlas.
    - Se reorganizó la estructura del proyecto moviendo el archivo index.js dentro de la carpeta 'src'.
    - Se crearon las carpetas 'models' y 'routes' para una mejor organización del código.
    - Se actualizó el script 'dev' en package.json para apuntar al archivo src/index.js.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+const http = require('http');
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'content-type': 'text/plain' });
+    res.end('Hola Mundo');
+});
+server.listen(5000);
