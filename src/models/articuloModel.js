2451cf10bd1718bc4b5f47f8c9b5ff2febf0bd01
valentinalopezgrc <lavaloga17@gmail.com>
2025-10-20 16:28:20 -0500

    Actualización de models/articuloModel.js

--- src/models/articuloModel.js
+++ src/models/articuloModel.js
@@ -1,15 +1,42 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
-const articuloSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  autores: { type: String, required: true },
-  anioPublicacion: { type: Number, required: true },
-  resumen: { type: String, required: true },
-  cantidadReferencias: { type: Number, required: true },
-  baseDatos: { type: String, required: true },
-  fuentePublicacion: { type: String, required: true },
-  enlace: { type: String, required: true },
-  pdf: { type: String, required: true },
-}, { timestamps: true });
+const articuloSchema = mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true,
+  },
+  autores: {
+    type: String,
+    required: true,
+  },
+  anioPublicacion: {
+    type: Number,
+    required: true,
+  },
+  resumen: {
+    type: String,
+    required: true,
+  },
+  cantidadReferencias: {
+    type: Number,
+    required: true,
+  },
+  baseDatos: {
+    type: String,
+    required: true,
+  },
+  fuentePublicacion: {
+    type: String,
+    required: true,
+  },
+  enlace: {
+    type: String,
+    required: true,
+  },
+  pdf: {
+    type: String,
+    required: true,
+  },
+});
 
-export default mongoose.model('Articulo', articuloSchema);
+module.exports = mongoose.model("Articulo", articuloSchema);
