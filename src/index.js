9a3cdfbf048c2cefa736f4ef95bcde009ab31346
valentinalopezgrc <lavaloga17@gmail.com>
2025-10-20 23:33:41 -0500

    Ultimas actualizaciones

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const parser = require("body-parser");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 3000;
 const articuloRoutes = require("./routes/articulos");
 const mongoose = require("mongoose");
 require("dotenv").config();
